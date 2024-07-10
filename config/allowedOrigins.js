46ad42b9e55460415003cb159b7f78608122124b
altai861 <altaizandanbileg@gmail.com>
2024-07-10 11:45:06 +0800

    origin update

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173',
-    'https://altai861.github.io',
+    'https://altai861.github.io/blog',
 ]
 
 module.exports = allowedOrigins
